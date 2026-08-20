6831d53546df364a59afdc62d07726a0ca6e5a25
Sleexy <hax_beixi@outlook.com>
2026-08-20 17:07:24 +0800

    0.2

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+}
+
+android {
+    namespace = "com.sdau.classschedule"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.sdau.classschedule"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 2
+        versionName = "V0.2"
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.core:core-ktx:1.13.0")
+}
